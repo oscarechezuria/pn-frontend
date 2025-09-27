53edb3d38adc182ded5cea2a3b16ef536e975961
oscar <echezuoscar@gmail.com>
2025-09-27 19:36:19 -0400

    feat: Se crearon los estilos globales para los colores, la tipografía y la fuente

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="bg-primary-100 text-heading">
+      Listo la primera configuración con los colores globales y la tipografía
+    </div>
+  );
+}
