3e11a9825dc1ec7039b71091bcc824cddbd07089
oscar <echezuoscar@gmail.com>
2025-10-04 21:49:07 -0400

    fix: Eliminando botón X del sidebar y ajustando el width en mobile a 182px

--- src/components/layout/dashboard/sidebarContent/index.tsx
+++ src/components/layout/dashboard/sidebarContent/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { TvMinimal, User, X, Settings } from "lucide-react"; 
+import { TvMinimal, User, X, Settings, LogOut } from "lucide-react"; 
 import { supabase } from "@/lib/supabaseClient";
 import { useRouter } from "next/navigation";
 
@@ -31,12 +31,8 @@ export default function Sidebar({ onSelect, sidebarOpen, pathname }: SidebarProp
   
 
   return (
-    <aside className={`flex flex-col justify-between gap-12 bg-secondary p-4 lg:pt-16 lg:w-64 lg:pl-12 lg:pr-4 fixed inset-0 z-50 transform transition-transform duration-300 ease-in-out ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} lg:relative lg:translate-x-0 `}>
+    <aside className={`flex flex-col justify-between gap-12 bg-secondary p-4 pt-16 lg:w-64 lg:pl-12 lg:pr-4 fixed inset-0 z-50 transform transition-transform duration-300 ease-in-out ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} lg:relative lg:translate-x-0 `}>
 
-        {/* Menu icon */}
-        <div className="flex justify-center lg:hidden">
-            <X size={36}  onClick={onSelect} className="text-secondary bg-primary rounded-full p-1 cursor-pointer" />
-        </div>
     
         {/* Sidebar Header */}
         <div className=" flex justify-center items-center gap-2">
@@ -44,7 +40,7 @@ export default function Sidebar({ onSelect, sidebarOpen, pathname }: SidebarProp
         </div>
         
         {/* Sidebar Content */}
-        <div className="flex flex-col gap-2 justify-center mx-auto">
+        <div className="flex flex-col gap-2 justify-center w-[182px] mx-auto lg:w-full">
             {sidebarItems.map(({ title, path, icon: Icon }) => (
               title !== "Cerrar Sesión" ? 
               (
@@ -62,8 +58,11 @@ export default function Sidebar({ onSelect, sidebarOpen, pathname }: SidebarProp
         </div>
 
          {/* footer content */}   
-          <div className="flex justify-center mx-auto">
-               <Link href="/" onClick={handleSignOut} className="flex justify-center bg-red-300 text-red-600 font-bold py-2 px-6 rounded-xl w-full">Cerrar Sesión</Link>
+          <div className="flex justify-center w-[182px] mx-auto lg:w-full">
+               <Link href="/" onClick={handleSignOut} className="flex justify-center bg-red-300 text-red-600 font-bold py-2 px-6 rounded-xl w-full">
+                 <LogOut size={24} className="mr-2"/>
+                 Cerrar Sesión
+               </Link>
           </div>
     </aside>
   );
